7ee99d4914b69bc3691a424acce718c40865e2c6
Riley <rileyjordan21@hotmail.com>
2026-02-19 17:50:49 +0000

    update

--- api/index.ts
+++ api/index.ts
@@ -40,12 +40,12 @@ app.use((req, res, next) => {
 
   const isMobileApp = !origin;
 
-  if (isMobileApp || (origin && (allowedOrigins.has(origin) || isLocalhost))) {
-    res.header("Access-Control-Allow-Origin", origin || "*");
-    res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
-    res.header("Access-Control-Allow-Headers", "Content-Type");
-    res.header("Access-Control-Allow-Credentials", "true");
-  }
+if (isMobileApp || (origin && (allowedOrigins.has(origin) || isLocalhost))) {
+  res.header("Access-Control-Allow-Origin", origin || "https://v0-uni-code.vercel.app");
+  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+  res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
+  res.header("Access-Control-Allow-Credentials", "true");
+}
 
   if (req.method === "OPTIONS") {
     return res.sendStatus(200);
